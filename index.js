f843a7246e1da7a1b7ac396030c22d408365f9b0
javoh1rqaxramonov <ubotirov22@.com>
2024-07-23 15:22:50 +0500

    finished

---
+++ index.js
@@ -0,0 +1,106 @@
+// 1-masala
+// let sum = 0;
+// for (let  i=10;i<=100;i++){
+//   if (i%2!==0) {
+//     sum+=i;
+//   }
+//   console.log(sum);
+// }
+
+// 2-masala 
+// let a =8;
+// let result =1;
+// for (let i = 1; i <= a; i++) {
+//   result *= i; 
+// }
+// console.log(result);
+
+// 3-masala 
+// 4-masala 
+// let sum =0;
+// for(let i=1;i<=20;i++){
+//   sum=sum+i**2;
+// }
+// console.log(sum);
+
+// 5-masala 
+// let a=1;
+// let b=2;
+// let c=3;
+// let d=4;
+// let e=5;
+// let max=0 ;
+// let min =0;
+// if (a>b || b>a) {
+//   max=a;
+//   max=b;
+// } else {
+//   min =a;
+//   min =b;
+// }
+// if (c>d || d>c) {
+//   max =c;
+//   max =d;
+// } else {
+//   min=c;
+//   min=d;
+// }
+// if (e>max|| max>e) {
+//   max=max;
+//   max=e;
+// } else {
+//   min=e;
+//   min=max
+// }
+// console.log(min);
+// console.log(max);
+
+// 8-masala
+// let res;
+// for(let i=1;i<=10;i++){
+//   if (i%2==0) {
+//     res=i**2;
+//   } else if (i%2==1) {
+//     res=i**3;
+//   }
+//   console.log(res);
+// }
+
+// 9-masala
+// let res;
+// for(let i =1;i<=50;i++){
+//   if (i%3==0 || i%5==0) {
+//     console.log(i);
+//   }
+// }
+
+// 1-masala 
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i); 
+//   if (i == 5) {
+//       break; 
+//   }
+// }
+
+// 2-masala 
+// for (let i = 1; i <= 10; i++) {
+//   if (i == 3 || i == 7) {
+//       continue; 
+//   }
+//   console.log(i); 
+// }
+
+
+// 3-masala 
+  // let a = 7
+
+  // if (a == 7) {
+  //     console.log("Topildi!"); 
+  //     break; 
+  // } else {
+  //     console.log("topilmadi"); 
+  // }
+
+  
+
+
